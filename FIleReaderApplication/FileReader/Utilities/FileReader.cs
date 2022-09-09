216fee70ba263de4cc92eb6d645c3ba14211de36
David Hansson <David.Hansson@aareon.com>
2022-09-09 12:25:16 +0200

    Added matching between filelines

--- FIleReaderApplication/FileReader/Utilities/FileReader.cs
+++ FIleReaderApplication/FileReader/Utilities/FileReader.cs
@@ -1,5 +1,6 @@
 ﻿using FileReader.Interfaces;
 using FileReader.Model;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -74,15 +75,19 @@ namespace FileReader.Utilities
             {
                 bool endOfFile;
                 string fileLine = !reader.EndOfStream ? null : reader.ReadLine();
-
+                string previousFileLine = null;
                 do
                 {
                     var matches = expression.Matches(fileLine ?? string.Empty);
                     if (matches.Any())
                         result.NumberOfMatches += matches.Count;
 
+                    previousFileLine = fileLine;
                     fileLine = reader.ReadLine();
                     endOfFile = reader.EndOfStream;
+
+                    if (this.MatchSearchTermBetweenFileLines(previousFileLine, fileLine, result.SearchTerm, expression))
+                        result.NumberOfMatches = result.NumberOfMatches + 1;
 
                     // The last line will not be scanned if the end of file is reached
                     if (endOfFile && fileLine != null)
@@ -92,6 +97,22 @@ namespace FileReader.Utilities
             }
 
             return result;
+        }
+
+        protected bool MatchSearchTermBetweenFileLines(string previousFileLine, string currentFileLine, string searchTerm, Regex matchingExpression)
+        {
+            try
+            {
+                var searchTermLength = searchTerm.Length;
+                var previousFileEnd = previousFileLine.Substring(previousFileLine.Length - searchTermLength -2);
+                var currentFileStart = currentFileLine.Substring(0, searchTermLength - 2);
+                
+                var isMatchedFullyInLineEndOrLineBeginning = matchingExpression.IsMatch(previousFileEnd) || matchingExpression.IsMatch(currentFileLine.Substring(0, searchTerm.Length));
+
+                return !isMatchedFullyInLineEndOrLineBeginning && matchingExpression.IsMatch($"{previousFileEnd}{currentFileStart}");
+            }
+            catch (ArgumentOutOfRangeException) { /* The substring indexes are either to small or large. This catch reduces uneeded checking*/ return false; }
+            catch (NullReferenceException) {/* One or more of the strings are null. They would not have matched anyway*/ return false; }
         }
     }
 }
