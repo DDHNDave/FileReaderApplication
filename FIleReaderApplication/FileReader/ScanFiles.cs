574cbb558b45740f271cd49a5ac3f1db42944dfa
David Hansson <David.Hansson@aareon.com>
2022-09-09 11:23:43 +0200

    added error handling to console application
    added read from console when no paths have been added

--- FIleReaderApplication/FileReader/ScanFiles.cs
+++ FIleReaderApplication/FileReader/ScanFiles.cs
@@ -1,5 +1,6 @@
 ﻿using FileReader.Interfaces;
 using FileReader.Utilities;
+using Microsoft.Extensions.Hosting;
 using System;
 using System.IO;
 using System.Linq;
@@ -31,7 +32,7 @@ namespace FileReader
             return result.Split("#")
                 .Where(path => !string.IsNullOrEmpty(path))
                 .Select(filePath => this.StandardizeFilePath(filePath))
-                .ToArray();
+                .ToArray(); 
         }
 
         public void Run(string[] arguments)
@@ -40,13 +41,25 @@ namespace FileReader
             if (cleanedUpCommandLineArguments == null || (cleanedUpCommandLineArguments?.Length ?? 0) == 0)
             {
                 Console.WriteLine("No filepath or filePaths have been specified");
-                return; // Exit method
+                this.StartInterActiveConsole();
             }
 
-            if (cleanedUpCommandLineArguments.Length > 1)
-                this.MessagePrinter.PrintFilesMatchingSearchTermsMessage(this.FileScanner.ScanFilesForSearchTerm(cleanedUpCommandLineArguments));
-            else
-                this.MessagePrinter.PrintFilesMatchingSearchTermsMessage(this.FileScanner.ScanFileForSearchTerm(cleanedUpCommandLineArguments[0]));
+            switch ((cleanedUpCommandLineArguments?.Length ?? 0))
+            {
+                case 0:
+                    Console.WriteLine("No filepath or filePaths have been specified");
+                    this.StartInterActiveConsole();
+                    break;
+
+                case 1:
+                    this.MessagePrinter.PrintFilesMatchingSearchTermsMessage(this.FileScanner.ScanFileForSearchTerm(cleanedUpCommandLineArguments[0]));
+                    break;
+
+                default:
+                    if((cleanedUpCommandLineArguments?.Length ?? 0) > 1)
+                        this.MessagePrinter.PrintFilesMatchingSearchTermsMessage(this.FileScanner.ScanFilesForSearchTerm(cleanedUpCommandLineArguments));
+                    break;
+            }
         }
         protected string StandardizeFilePath(string attemptedPath)
         {
@@ -55,6 +68,37 @@ namespace FileReader
                 normalizedPath = Path.Combine(normalizedPath, part);
 
             return normalizedPath;
+        }
+
+        protected void StartInterActiveConsole()
+        {    
+            try
+            {
+                Regex isYesExpression = new Regex(@"(yes)|(y)", RegexOptions.IgnoreCase);
+                Console.WriteLine("No file or path to file have been specified as argument to the Program. Do yoy want to specifiy a file? \n");
+                Console.WriteLine("Enter y or yes to continue. To quit or close the application, type q, quit or anything other than yes \n ");
+                var answer = Console.ReadLine();
+
+                if (!isYesExpression.IsMatch(answer))
+                    Environment.Exit(0);
+
+                if (isYesExpression.IsMatch(answer))
+                {
+                    Console.WriteLine("Enter directory or file path below \n");
+                    var filePath = this.CleanUpPathCommandLineArguments(new string[] { Console.ReadLine() ?? "" });
+                    if(filePath.Length > 0)
+                        this.MessagePrinter.PrintFilesMatchingSearchTermsMessage(this.FileScanner.ScanFileForSearchTerm(filePath[0]));
+
+                    return;
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occured when a file with the entered filePath scanned, Details: {0}", e.Message);
+                // Exit method
+                Environment.Exit(0);
+            }
         }
     }
 }
