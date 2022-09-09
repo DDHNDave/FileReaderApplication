11a1b4dedeb9737281e41e7034f10c13cbfd26cd
David Hansson <David.Hansson@aareon.com>
2022-09-09 10:35:01 +0200

    added processing of command line arguments

--- FIleReaderApplication/FileReader/ScanFiles.cs
+++ FIleReaderApplication/FileReader/ScanFiles.cs
@@ -1,6 +1,8 @@
 ﻿using FileReader.Interfaces;
 using FileReader.Utilities;
 using System;
+using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace FileReader
@@ -17,26 +19,42 @@ namespace FileReader
             this.MessagePrinter = messagePrinter;
         }
 
-        protected string processPathCommandLineArguments(string[] arguments)
+        protected string[] CleanUpPathCommandLineArguments(string[] arguments)
         {
-            Regex normalizePathDividerExpression = new Regex(@"[\\\/]", RegexOptions.IgnoreCase);
+            Regex normalizePathDividerExpression = new Regex(@"[\\\/]+", RegexOptions.IgnoreCase);
+            Regex removePathNameExpression = new Regex(@"(\s{0,}\w{0,}path\w{0,}\s{0,}\=\s{0,})", RegexOptions.IgnoreCase);
+            string result = string.Empty;
 
-            foreach(string path in arguments)
+            foreach (string path in arguments)
+                result += $"{normalizePathDividerExpression.Replace(removePathNameExpression.Replace(path, string.Empty), "-DIRECTORY-")}#";
 
+            return result.Split("#")
+                .Where(path => !string.IsNullOrEmpty(path))
+                .Select(filePath => this.StandardizeFilePath(filePath))
+                .ToArray();
         }
 
         public void Run(string[] arguments)
         {
-            if (arguments == null || (arguments?.Length ?? 0) == 0)
+            var cleanedUpCommandLineArguments = this.CleanUpPathCommandLineArguments(arguments);
+            if (cleanedUpCommandLineArguments == null || (cleanedUpCommandLineArguments?.Length ?? 0) == 0)
             {
                 Console.WriteLine("No filepath or filePaths have been specified");
                 return; // Exit method
             }
 
-            if (arguments.Length > 1)
-                this.MessagePrinter.PrintFilesMatchingSearchTermsMessage(this.FileScanner.ScanFilesForSearchTerm(arguments));
+            if (cleanedUpCommandLineArguments.Length > 1)
+                this.MessagePrinter.PrintFilesMatchingSearchTermsMessage(this.FileScanner.ScanFilesForSearchTerm(cleanedUpCommandLineArguments));
             else
-                this.MessagePrinter.PrintFilesMatchingSearchTermsMessage(this.FileScanner.ScanFileForSearchTerm(arguments[0]));
+                this.MessagePrinter.PrintFilesMatchingSearchTermsMessage(this.FileScanner.ScanFileForSearchTerm(cleanedUpCommandLineArguments[0]));
+        }
+        protected string StandardizeFilePath(string attemptedPath)
+        {
+            string normalizedPath = string.Empty;
+            foreach (string part in attemptedPath.Split("-DIRECTORY-"))
+                normalizedPath = Path.Combine(normalizedPath, part);
+
+            return normalizedPath;
         }
     }
 }
